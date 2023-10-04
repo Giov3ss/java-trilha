110ea8f0a9676162fd08e735da44e4fb324209b0
Giov3ss <giovani.sp25@gmail.com>
2023-10-04 22:26:34 +0100

    tipos de variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Usando double e diferenciando numeros usando a convencao Americana ".";
+        // 2.500 = (2.5) // 2500 = 2500;
+        double salarioMinimo = 2500.33; 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        // usando "Final" a palavra nao podera ser alterada;
+        // essa variavel sera do tipo constante, use sempre CAIXA ALTA para variaveis constante;
+        final double VALOR_DE_PI = 3.14; 
     }
 }
