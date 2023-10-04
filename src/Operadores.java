858406945863d442d31f91fa14d8cc42769ae8a4
Giov3ss <giovani.sp25@gmail.com>
2023-10-04 23:36:38 +0100

    finalizando exemplos de operadores

---
+++ src/Operadores.java
@@ -0,0 +1,102 @@
+public class Operadores {
+    public static void main(String [] args) {
+
+        /* OPERADORES ARITMETICOS
+         * == : Igual // 
+         * = : Atribuicao // String name = "Giovani"
+         * + : Adicao // 10.5 + 15.7
+         * - : Subtracao // 113 - 25
+         * * : Multiplicacao // 20 * 7
+         * / : Divisao // 15 / 3
+         * % : Modulo (ou RESTO da divisao) // 18 % 3 
+         */
+
+         /* OPERADORES UNARIOS
+          * (+) : positivo 
+          * (-) : negativo
+
+          * (++) : Incrementa
+                int numero = 5;
+                // Repeticao
+                numero++ 
+                Output: 6
+        
+          * (--) : Decrementa
+                int numero = 5;
+                // Repeticao
+                numero-- 
+                Output: 4
+
+          * (!) : Negacao
+                boolean variavel = true
+                variavel = !variavel 
+                Output: false 
+          */
+
+          /* OPERADORES TERNARIOS
+           * O operador ternario e apresentado pelos simbolos "? :" utilizados na seguinte sintaxe:
+           * <Expressao Condicional> ? (IF) <Caso condicao seja TRUE> : (se nao) <Caso a condicao seja FALSE>
+           * 
+           * Exemplo de condicional utilizando IF/ELSE
+           * 
+           * int a, b;
+           * a = 5
+           * b = 6
+           * String resultado = ""
+           * 
+           * if (a == b)
+           *    resultado = "verdadeiro"
+           * else 
+           *    resultado = "Falso"
+           *
+           * Output: Falso (pois A nao e igual a B)
+           * 
+           * Usando o operador TERNARIO:
+           * 
+           * int a, b;
+           * a = 5;
+           * b = 6;
+           * 
+           * String resultado = a == b ? "verdadeiro" : "falso"
+           * 
+           * Output: falso
+           * 
+           */
+
+           /*
+            * == : Igual
+            * != : Diferente
+            * > : Maior que 
+            * >= : Maior ou igual
+            * < : Menor que
+            * <= : Menor ou igual 
+            */
+
+            /* OPERADORES LOGICOS
+             * 
+             * && Operador logico "E" (se alguma das condicoes for false, retorna false)
+             * || Operador logico "OU" (se uma das condicoes for true, retorna true )
+             * 
+             * EXEMPLO:
+             * 
+             * boolean condicao1 = true
+             * boolean condicao2 = false
+             * 
+             * if (condicao1 && condicao2) {
+             *    IMPRIME("As duas condicoes sao verdadeiras?")
+             * }
+             * 
+             * Output: false
+             * 
+             * ---------------------
+             * 
+             * if (condicao1 || condicao2) {
+             *     IMPRIME("uma das condicoes e verdadeira?")
+             * } 
+             * 
+             * Output: true
+             * 
+             * 
+             */
+    }
+}
