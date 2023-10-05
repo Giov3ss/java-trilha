1839aaa92e121f3e072d32914aa1a5105f54817d
Giov3ss <giovani.sp25@gmail.com>
2023-10-05 12:22:26 +0100

    adicionando aula de metodos

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,5 +1,15 @@
 public class Metodos {
     public static void main(String [] args) {
-        
+        /* CRITERIO DE NOMEACAO DE METODOS
+         * Deve ser nomeado como verbo;
+         * Seguir o padrao camelCase;
+         * 
+         * ex: 
+         * somar(int n1, int n2)
+         * abrirConexao() {}
+         * concluirProcessament() {}
+         * findById(int id){}
+         * 
+         */
     }
 }
